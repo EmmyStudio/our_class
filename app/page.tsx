8d64c518eef7f2fc1c16259e473f8a3c75f0e9f1
Emmystudio <olatolanrewaju81@gmail.com>
2026-09-05 13:11:52 +0100

    merge conflicts fixed

--- app/page.tsx
+++ app/page.tsx
@@ -59,22 +59,20 @@ export default function Home() {
         </div>
       </main>
 
-      <div className="flex w-full items-center justify-center border-t border-zinc-200 bg-white/90 px-6 py-4 shadow-sm backdrop-blur dark:border-zinc-800 dark:bg-black/80">
-      <p className="text-sm text-zinc-600 dark:text-zinc-400">
-        New Horizons Nigeria
-        </p>
-      </div>
-
-
-
-      
-      
+        <div className="flex w-full items-center justify-center border-t border-zinc-200 bg-white/90 px-6 py-4 shadow-sm backdrop-blur dark:border-zinc-800 dark:bg-black/80">
+        <p className="text-sm text-zinc-600 dark:text-zinc-400">
+          New Horizons Nigeria
+          </p>
+        </div>
+ 
     </div>
     <Second/>
     <Third/>
     <Fourth/>
     <Fifth/>
     <Sixth/>
+    <Seventh/>
+    <Footer/>
     </>
   );
 }
