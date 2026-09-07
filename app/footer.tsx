350187c756fe284d8b98ee63281a6c06b8e5ae48
Emmystudio <olatolanrewaju81@gmail.com>
2026-09-07 09:51:06 +0100

    final

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,65 +1,74 @@
 export function Footer() {
   return (
-    <div className="flex flex-row gap-4 pt-0 ">
-      <div className="flex-1 flex-col gap-4 pl-10 max-w-90 pr-10 p-10">
-              <h5 className="text-3xl font-bold text-zinc-600 dark:text-zinc-400" id="bigText">
-                New Horizons Nigeria.
-              </h5>
-              <h6 className="text-zinc-600 dark:text-zinc-400" id="bigText">
-                Securing valuable legac enterprises through strcutured operation, goverance, and institutional financial architectures
-              </h6>
-            
+    <div className="bg-black-700 text-white">
+        <div className="flex flex-row gap-4 pt-0 ">
+        <div className="flex-1 flex-col gap-4 pl-10 max-w-90 pr-10 p-10">
+                <h5 className="text-3xl font-bold text-zinc-600 dark:text-zinc-400" id="bigText">
+                  New Horizons Nigeria.
+                </h5>
+                <h6 className="text-zinc-600 dark:text-zinc-400" id="bigText">
+                  Securing valuable legac enterprises through strcutured operation, goverance, and institutional financial architectures
+                </h6>
+              
+        </div>
+        <div className="flex-1 flex-col gap-4 max-w-90 p-9">
+          <h5 className="text-2xl text-zinc-600 dark:text-zinc-400" id="bigText">
+            Support
+          </h5>
+          <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
+            About Us
+          </a>
+          <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
+            Contact Us
+          </a>
+          <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
+            Our Careers
+          </a>
+        </div>
+        <div className="flex-1 flex-col gap-4 max-w-90 p-9">
+          <h5 className="text-2xl text-zinc-600 dark:text-zinc-400" id="bigText">
+            Legal
+          </h5>
+          <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
+            Privacy Policy
+          </a>
+          <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
+            Terms of Service
+          </a>
+          <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
+            Cookie Policy
+          </a>
+          <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
+            Accessibility
+          </a>
+        </div>
+        <div className="flex-1 flex-col gap-4 max-w-90 p-9">
+          <h5 className="text-2xl text-zinc-600 dark:text-zinc-400" id="bigText">
+            Support
+          </h5>
+          <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
+            Help
+          </a>
+          <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
+            Contact Us
+          </a>
+          <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
+            FAQs
+          </a>
+          <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
+          Live Chat
+          </a>
+        </div>
+        
+      
       </div>
-      <div className="flex-1 flex-col gap-4 max-w-90 p-9">
-        <h5 className="text-2xl text-zinc-600 dark:text-zinc-400" id="bigText">
-          Support
-        </h5>
-        <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
-          About Us
-        </a>
-        <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
-          Contact Us
-        </a>
-        <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
-          Our Careers
-        </a>
+      <div className="flex justify-left items-left p-10 border-t border-solid border-zinc-500">
+      <img src="/footer.webp" alt="NH Nigeria" className="w-auto h-auto" />
       </div>
-      <div className="flex-1 flex-col gap-4 max-w-90 p-9">
-        <h5 className="text-2xl text-zinc-600 dark:text-zinc-400" id="bigText">
-          Legal
-        </h5>
-        <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
-          Privacy Policy
-        </a>
-        <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
-          Terms of Service
-        </a>
-        <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
-          Cookie Policy
-        </a>
-        <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
-          Accessibility
-        </a>
-      </div>
-      <div className="flex-1 flex-col gap-4 max-w-90 p-9">
-        <h5 className="text-2xl text-zinc-600 dark:text-zinc-400" id="bigText">
-          Support
-        </h5>
-        <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
-          Help
-        </a>
-        <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
-          Contact Us
-        </a>
-        <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
-          FAQs
-        </a>
-        <a href="https://github.com/EmmyStudios" className="block text-7xs text-white dark:text-white hover:text-green-500 transition-colors" id="bigText">
-        Live Chat
-        </a>
-      </div>
-
+    
     </div>
+    
+
     
   );
 }
