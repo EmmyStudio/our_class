cf4ebbd80d6627bde3b63132493fb4ba24758361
Aleem Ahmed <ikealeem18@gmail.com>
2026-09-10 12:32:58 +0100

    Finished the sixth component

--- app/ike/page.tsx
+++ app/ike/page.tsx
@@ -85,7 +85,7 @@ export default function () {
                 <ThirD />
                 <FourtH />
                 <FiftH />
-                <SixtH />
+                <SixtH /> 
             </div>
 
         </>
